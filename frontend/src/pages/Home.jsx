d698fbaa94308d748548b51443c770f7c3faffa8
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2024-11-01 00:52:06 +0600

    Add About component and update Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,16 @@ import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
-
+import icon02 from "../assets/images/icon02.png";
+import icon03 from "../assets/images/icon03.png";
+import {Link} from "react-router-dom";
+import {BsArrowRight} from "react-icons/bs";
+import About from '../components/About/About';
 
 const Home = () => {
-  return <>
-  {/*==== hero section ==== */}
+  return (
   <>
+  {/*==== hero section ==== */}
   <section className='hero__section pt-[60px] 2xl:h-[800px]'>
     <div className='container'>
       <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
@@ -73,7 +77,7 @@ const Home = () => {
       </div>
     </div>
   </section>
-  {/*==== hero section ==== */}
+  {/*==== hero section end ==== */}
   <section>
     <div className='container'>
       <div className=' lg:w-[470px] mx-auto'>
@@ -95,22 +99,75 @@ const Home = () => {
 
           <div className='mt-[30px]'>
             <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
-              Find a doctor
+              Find a Doctor
             </h2>
             <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
               World-class care for everyone. Our health System offers unmatched,
               expert health care.
             </p>
-            
+            <Link 
+              to="/doctors" 
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px]
+              ms-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+            >
+              <BsArrowRight className=' group-hover:text-white w-6 h-5'/>
+            </Link>
           </div>
         </div>
+
+        <div className='py-[30px] px-5'>
+          <div className='flex items-center justify-center'>
+            <img src={icon02} alt="" />
+          </div>
+
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+              Find a Location
+            </h2>
+            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+            <Link 
+              to="/doctors" 
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px]
+              ms-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+            >
+              <BsArrowRight className=' group-hover:text-white w-6 h-5'/>
+            </Link>
+          </div>
+        </div>
+
+        <div className='py-[30px] px-5'>
+          <div className='flex items-center justify-center'>
+            <img src={icon03} alt="" />
+          </div>
+
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+              Book Appointment
+            </h2>
+            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+            <Link 
+              to="/doctors" 
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px]
+              ms-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+            >
+              <BsArrowRight className=' group-hover:text-white w-6 h-5'/>
+            </Link>
+          </div>
+        </div>
+        
       </div>
     </div>
   </section>
-  </>
 
+  <About />
   </>
-  
-}
+  );
+};
 
 export default Home
